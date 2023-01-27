e36375d96663eafe7e12e2a81f77fb0fe0112516
coraskid <m255844@usna.edu>
2023-01-27 18:56:52 -0500

    msg

--- lab03/Lab03.java
+++ lab03/Lab03.java
@@ -8,22 +8,33 @@ public class Lab03{
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
     String command = in.next();
-    while (command != "done"){
-      if(command == "add"){
-
+    boolean boxCreated = false;
+    while (!command.equals("done")){
+      if(command.equals("add")){
+        Point p = new Point(in);
+        if(!boxCreated){
+          Box B = new Box(p);
+          boxCreated = true;
+        }
+        else{
+          B.growBy(p);
+        }
+        System.out.println("add");
       }
-      else if(command == "box"){
-
+      else if(command.equals("box")){
+        System.out.println(B.toString());
+        System.out.println("box");
       }
-      else if(command == "map"){
-
+      else if(command.equals("map")){
+        
+        System.out.println("mpa");
       }
       else {
-        System.out.println("Error! Unknown command \"" + command + "\"");
+        System.out.println("Error! Unknown command \"" + command + "\"!");
       }
 
       System.out.println(command);
-      command = is.next();
+      command = in.next();
     }
 
   }
