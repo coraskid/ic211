beb929128dd02f5674dcf338b95517c30b695a88
coraskid <m255844@usna.edu>
2023-01-28 10:08:36 -0500

    msg

--- lab03/Lab03.java
+++ lab03/Lab03.java
@@ -1,8 +1,7 @@
 import java.util.*;
 
 public class Lab03{
-  private Box Hi;
-
+  private Box B;
   private void add(Scanner in){
         
 
@@ -18,43 +17,34 @@ public class Lab03{
     //if(K == null){
     //  System.out.println("hii");
     //}
-    System.out.println(Hi.toString());
+    //System.out.println(Hi.toString());
     //K.growBy(f);
     //System.out.println(K.toString());
    
 
     Scanner in = new Scanner(System.in);
     String command = in.next();
+
     boolean boxCreated = false;
-    //Box B = new Box;
+    var B;
+    //if (Lab03.B == null){
+    //  System.out.println("PLEASE");
+    //}
     //System.out.println(B.toString());
     while (!command.equals("done")){
-      if(command.equals("add") && !boxCreated){
-        
+      if(command.equals("add")){
         Point p = Point.read(in);
-        //B = new Box(p);
-        //if(!boxCreated){
-        //  B = new Box(p);
-        //  boxCreated = true;
-        //}
-        //else{
-        //  //B.growBy(p);
+        Box B = new Box(p);
+        //else if(B==null){
+          //System.out.println("no")
+          //B.growBy(p);
         //}
         System.out.println("add");
+        break;
       }
-      else if(command.equals("add")){
-        Point q = Point.read(in);
+      else if(command.equals("box") || command.equals("map")){
+        System.out.println("Error, add point first");
         //B.growBy(q);
-      }
-      else if(command.equals("box")){
-        if(!boxCreated){
-          //System.out.println(B.toString());
-        }
-        System.out.println("box");
-      }
-      else if(command.equals("map")){
-        
-        System.out.println("mpa");
       }
       else {
         System.out.println("Error! Unknown command \"" + command + "\"!");
@@ -63,6 +53,8 @@ public class Lab03{
       System.out.println(command);
       command = in.next();
     }
+    System.out.println("OUt?");
+    System.out.println(B.toString);
 
   }
 }
