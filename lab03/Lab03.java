f0ae3cefb6eafdb4d4ab47b40f8587e63a0ca909
coraskid <m255844@usna.edu>
2023-01-27 22:14:13 -0500

    msg

--- lab03/Lab03.java
+++ lab03/Lab03.java
@@ -1,28 +1,55 @@
 import java.util.*;
 
 public class Lab03{
+  private Box Hi;
+
+  private void add(Scanner in){
+        
 
 
+  }
 
 
   public static void main(String[] args){
+    //Point l = new Point(3,4);
+    Point f = new Point(4,3);
+    //Hi = new Box(f);
+    //Hi.growBy(f);
+    //if(K == null){
+    //  System.out.println("hii");
+    //}
+    System.out.println(Hi.toString());
+    //K.growBy(f);
+    //System.out.println(K.toString());
+   
+
     Scanner in = new Scanner(System.in);
     String command = in.next();
     boolean boxCreated = false;
+    //Box B = new Box;
+    //System.out.println(B.toString());
     while (!command.equals("done")){
-      if(command.equals("add")){
-        Point p = new Point(in);
-        if(!boxCreated){
-          Box B = new Box(p);
-          boxCreated = true;
-        }
-        else{
-          B.growBy(p);
-        }
+      if(command.equals("add") && !boxCreated){
+        
+        Point p = Point.read(in);
+        //B = new Box(p);
+        //if(!boxCreated){
+        //  B = new Box(p);
+        //  boxCreated = true;
+        //}
+        //else{
+        //  //B.growBy(p);
+        //}
         System.out.println("add");
       }
+      else if(command.equals("add")){
+        Point q = Point.read(in);
+        //B.growBy(q);
+      }
       else if(command.equals("box")){
-        System.out.println(B.toString());
+        if(!boxCreated){
+          //System.out.println(B.toString());
+        }
         System.out.println("box");
       }
       else if(command.equals("map")){
