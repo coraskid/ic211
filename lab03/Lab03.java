1af6f3abc5a689eaa1dec0b0308cf0ef5b3e85d1
coraskid <m255844@usna.edu>
2023-01-30 18:20:51 -0500

    msg

--- lab03/Lab03.java
+++ lab03/Lab03.java
@@ -1,9 +1,20 @@
 import java.util.*;
-
+/**
+ * This class manipulates and prints boxes and maps points with in boxes
+ * It can handle 4 different commands
+ * add - adds a point to the box (must be followed by two doubles/ints)
+ * box - prints out the min and max representation of the box
+ * map - maps the ratio of a given point and prints the ratios x and y value
+ *     - (must be followed by two doubles/ints)
+ * done - exit out of the program
+ * @author cora skidmore
+ */
 public class Lab03{
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
     String command = in.next();
+    //Checks for first add statement to create box without scope errors
+    //Now understand how to initialize as null
     while (!command.equals("done")){
       if(command.equals("add")){
                 break;
@@ -19,9 +30,11 @@ public class Lab03{
     if(command.equals("done")){
       return;
     }
+    //creates box
     Point p = Point.read(in);
     Box B = new Box(p);
     command = in.next();
+    //continues with any other function inputed into the system
     while(!command.equals("done")){
       if(command.equals("add")){
         B.add(in);
