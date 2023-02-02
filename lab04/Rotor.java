37b8d5bf169d607454f273aa6da6d113109a8a28
coraskid <m255844@usna.edu>
2023-02-01 21:30:38 -0500

    msg

---
+++ lab04/Rotor.java
@@ -0,0 +1,43 @@
+import java.util.*;
+/**
+ *
+ */
+public class Rotor{
+  private String order;
+  private char top;
+
+  public Rotor(String s){
+    this.order = s;
+    this.top = s[0];
+  }
+  public Rotor(String s, char t){
+    this.order = s;
+    this.top = t;
+    while(s[0] != t){
+      this.clockwise();
+    }
+
+  }
+
+  public void clockwise(){
+    String temp = this.order;
+    temp[0] = this.order[this.order.length()-1];
+    for(int i = 1; i < this.order.length(); i++){
+      temp[i] = this.order[i-1];
+    }
+    this.order = temp;
+  }
+
+  public static void main(String[] args){
+    Rotor r = new Rotor("asdfghjkl", 'd');
+    System.out.println(r.order);
+
+
+  }
+
+
+
+
+
+
+}
