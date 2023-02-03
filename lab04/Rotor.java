a96abe5332280779490f7e2b624961505fbe474b
coraskid <m255844@usna.edu>
2023-02-02 23:31:32 -0500

    msg

--- lab04/Rotor.java
+++ lab04/Rotor.java
@@ -6,30 +6,39 @@ public class Rotor{
   private String order;
   private char top;
 
-  public Rotor(String s){
-    this.order = s;
-    this.top = s[0];
-  }
+  //public Rotor(String s){
+    //this.order = s;
+    //this.top = s[0];
+  //}
   public Rotor(String s, char t){
     this.order = s;
     this.top = t;
-    while(s[0] != t){
+    while(s.charAt(0) != t){
       this.clockwise();
     }
 
   }
-
+  //charAt(int index);
+  //indexOf(char c);
+  //lastIndexOf(char c);
+  //split(String regex);
+  //concat(String end);
+  //toCharArray();
+  //valueOf(char[] data) - returns string
   public void clockwise(){
-    char[] temp = this.order;
-    temp[0] = this.order[this.order.length()-1];
-    for(int i = 1; i < this.order.length(); i++){
-      temp[i] = this.order[i-1];
+    int len = this.order.length(); 
+    char[] arr = this.order.toCharArray();
+    char temp = arr[len -1];
+    for(int j = len -1; j >= 1; j--){
+      arr[j] = arr[j-1];
     }
-    this.order = temp;
+    arr[0] = temp;
+    this.order = String.valueOf(arr);
   }
 
   public static void main(String[] args){
     Rotor r = new Rotor("asdfghjkl", 'd');
+    r.clockwise();
     System.out.println(r.order);
 
 
