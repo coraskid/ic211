b5b50cc0507b47e84a4d9d37a87d04dfc78c2c14
coraskid <m255844@usna.edu>
2023-02-04 12:10:30 -0500

    msg

--- lab04/Enigma.java
+++ lab04/Enigma.java
@@ -1,7 +1,25 @@
 import java.util.*;
 
 public class Enigma{
-  
+  private static final String[] roters = {"#GNUAHOVBIPWCJQXDKRYELSZFMT", 
+                                          "#EJOTYCHMRWAFKPUZDINSXBGLQV", 
+                                          "#BDFHJLNPRTVXZACEGIKMOQSUWY", 
+                                          "#NWDKHGXZVRIFJBLMAOPSCYUTQE", 
+                                          "#TGOWHLIFMCSZYRVXQABUPEJKND"}; 
+  //private static final String r2 = "#EJOTYCHMRWAFKPUZDINSXBGLQV";
+  //private static final String r3 = "#BDFHJLNPRTVXZACEGIKMOQSUWY";
+  //private static final String r4 = "#NWDKHGXZVRIFJBLMAOPSCYUTQE";
+  //private static final String r5 = "#TGOWHLIFMCSZYRVXQABUPEJKND";
+
+  private Rotor inner;
+  private Rotor middle;
+  private Rotor outer;
+
+  public Enigma(int in, int mi, int out, String start){
+    this.inner = new Rotor(roters[in-1], start.charAt(0));
+    this.middle = new Rotor(roters[mi-1], start.charAt(1));
+    this.outer = new Rotor(roters[out-1], start.charAt(2));
+  }
 
   public String encrypt(String msg){
 
