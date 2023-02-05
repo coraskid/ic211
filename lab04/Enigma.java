e7389d04064302bc6f8384819a7f933cbc3e2794
coraskid <m255844@usna.edu>
2023-02-04 19:45:38 -0500

    msg

--- lab04/Enigma.java
+++ lab04/Enigma.java
@@ -1,43 +1,111 @@
 import java.util.*;
-
+/**
+ * This class represents the function of an enigma machine and is able to
+ * encrypt and decrypt messages
+ * @author cora skidmore
+ */
 public class Enigma{
   private static final String[] roters = {"#GNUAHOVBIPWCJQXDKRYELSZFMT", 
                                           "#EJOTYCHMRWAFKPUZDINSXBGLQV", 
                                           "#BDFHJLNPRTVXZACEGIKMOQSUWY", 
                                           "#NWDKHGXZVRIFJBLMAOPSCYUTQE", 
                                           "#TGOWHLIFMCSZYRVXQABUPEJKND"}; 
-  //private static final String r2 = "#EJOTYCHMRWAFKPUZDINSXBGLQV";
-  //private static final String r3 = "#BDFHJLNPRTVXZACEGIKMOQSUWY";
-  //private static final String r4 = "#NWDKHGXZVRIFJBLMAOPSCYUTQE";
-  //private static final String r5 = "#TGOWHLIFMCSZYRVXQABUPEJKND";
-
   private Rotor inner;
   private Rotor middle;
   private Rotor outer;
 
+  /**
+   * Constructor
+   * Takes 3 rotor choices (taken from the 5 set rotor arrangements), and where
+   * the top is supposed to be and establishes the three rotors that will be
+   * used in the enigma
+   * @param in inner rotor number
+   * @param mi middle rotor number
+   * @param out outer rotor number
+   * @param start a string with 3 letters representing the top of each rotor in
+   * order (inner, middle, outer)
+   */
   public Enigma(int in, int mi, int out, String start){
     this.inner = new Rotor(roters[in-1], start.charAt(0));
     this.middle = new Rotor(roters[mi-1], start.charAt(1));
     this.outer = new Rotor(roters[out-1], start.charAt(2));
   }
-
+  /**
+   * Prints the current rotor arrangements
+   * Mainly used for debuging
+   */
+  private void getCur(){
+    System.out.println(this.inner.getO());
+    System.out.println(this.middle.getO());
+    System.out.println(this.outer.getO());
+    return; 
+  }
+  /**
+   * Finds the corresponding char on another rotor given two rotors and a char
+   * @param c the char that you have
+   * @param from the rotor you are starting at
+   * @param to the rotor you are going to
+   * @return the Char found at the 'to' rotor that matches
+   */
+  private static char match(char c, Rotor from, Rotor to){
+    int i = from.index(c);
+    char newC = to.atIndex(i);
+    return newC;
+  }
+  /**
+   * Encrypts a message based on the current enigma set up
+   * @param msg The message you want to encrypt
+   */
   public String encrypt(String msg){
-
+    int len = msg.length();
+    char[] encMsg = new char[len];
+    
+    for(int i = 0; i < len; i++){
+      char stepOne = match(msg.charAt(i), this.inner, this.outer);
+      char stepTwo = match(stepOne, this.middle, this.outer);
+      encMsg[i] = stepTwo;  
+      
+      this.inner.clockwise();
+      if (this.inner.atIndex(0) == this.inner.getT()){
+        this.middle.clockwise();
+        if (this.middle.atIndex(0) == this.middle.getT()){
+          this.outer.clockwise();
+        }
+      }
+    }
+    return String.valueOf(encMsg);
   }
-
+  /**
+   * Decrypts a message based on the current enigma set up
+   * @param msg The message you want decrypted
+   */
   public String decrypt(String msg){
+   // String set roters = this.encrypt(msg);
 
+    int len = msg.length();
+    char[] decMsg = new char[len];
+      
+    for(int i = 0; i < len; i++){
+      char stepOne = match(msg.charAt(i), this.outer, this.middle);
+      char stepTwo = match(stepOne, this.outer, this.inner);
+      decMsg[i] = stepTwo;
+     
+      this.inner.clockwise();
+      if (this.inner.atIndex(0) == this.inner.getT()){
+        this.middle.clockwise();
+        if (this.middle.atIndex(0) == this.middle.getT()){
+          this.outer.clockwise();
+        }
+      }
+    }
+    return String.valueOf(decMsg);
   }
 
-
-
-
-
-
-
-
-
-
-
+  public static void main(String[] args){
+    Enigma t = new Enigma(1,2,3, "###");
+    t.getCur();
+    System.out.println(t.decrypt("XXMMJ#UBHRWSHYSSTGWIGMMMAKTFSWZD#PU#CZIQADCDQY#NHN#SJVJTKZVVHOZBABLYMWBWWLNGAWWXEBWNXQQBQQRNMJGYXRYBKISBBFHGOO"));
+    t.getCur();
+  }
 
 }
--- lab04/Rotor.java
+++ lab04/Rotor.java
@@ -1,15 +1,29 @@
 import java.util.*;
 /**
- *
+ * This class represents and establishes rotors in their correct position, it
+ * also has the ability to rotate a rotor clockwise or counterclockwise by one
+ * @author cora skidmore
  */
 public class Rotor{
   private String order;
   private char top;
-
+  /**
+   * Constructor
+   * Takes a string that will represent the order of the rotor and uses the
+   * first letter in the string to represent the top
+   * @param s Order of letters on the rotor represented by a string
+   */
   public Rotor(String s){
     this.order = s;
     this.top = s.charAt(0);
   }
+  /**
+   * Constructor (more useful)
+   * Takes a string that represents the order of the rotor and a char that will
+   * be start of the rotor and adjusts the rotor accordingly
+   * @param s Order of letters on the rotor represended by a string
+   * @param t Char that will be the top of the rotor
+   */
   public Rotor(String s, char t){
     this.order = s;
     this.top = t;
@@ -17,20 +31,22 @@ public class Rotor{
       this.clockwise();
     }
   }
-
-  public String getO(){
+  /**
+   * A getter method for the order of the rotor
+   * Not yet needed in another class, remains private
+   */
+  private String getO(){
     return this.order;
   }
+  /**
+   * A getter method for the char that was originally on top 
+   */
   public char getT(){
     return this.top;
   }
-  //charAt(int index);
-  //indexOf(char c);
-  //lastIndexOf(char c);
-  //split(String regex);
-  //concat(String end);
-  //toCharArray();
-  //valueOf(char[] data) - returns string
+  /**
+   * Rotates the rotor by one clockwise
+   */
   public void clockwise(){
     int len = this.order.length(); 
     char[] arr = this.order.toCharArray();
@@ -42,11 +58,32 @@ public class Rotor{
     this.order = String.valueOf(arr);
   }
   
+  /**
+   * Rotates the rotor by one clockwise
+   */
+  public void ccw(){
+    int len = this.order.length(); 
+    char[] arr = this.order.toCharArray();
+    char temp = arr[0];
+    for(int j = 0; j < len-1; j++){
+      arr[j] = arr[j+1];
+    }
+    arr[len-1] = temp;
+    
+
+  }
+  /**
+   * Finds the index of a given char in the current order of the rotor
+   * @param c The char that we want the index of
+   */
   public int index(char c){
     int ind = this.order.indexOf(c);
     return ind;
   }
-
+  /**
+   * Finds the char of a given index in the current order of the rotor
+   * @param index The int that we want the char at
+   */
   public char atIndex(int index){
     char c = this.order.charAt(index);
     return c;
