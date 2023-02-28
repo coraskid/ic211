d53c92834c590e27e056afe2ac9bd04653d0e2e8
coraskid <m255844@usna.edu>
2023-02-28 17:01:21 -0500

    msg

---
+++ HW/polyCar/Car.java
@@ -0,0 +1,22 @@
+public class Car extends Vehicle {
+  private int numWheels;
+  private boolean canfly;
+  private String noise = "vroom";
+
+
+  public Car(String type, String model, int mpg){
+    super(type, model, mpg);
+    this.numWheels = 4;
+    this.canfly = false;
+  }
+
+  public static Car read(Scanner sc){
+    return new Car("Car", sc.next(), sc.next())
+  }
+
+  public String toString() {
+    return super.toString() + " " + noise + " does not fly with " + numWheels + " wheels.";  
+  }
+
+
+}
