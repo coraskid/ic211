af2db1e55f28a84a085e9074bda94343196bad37
coraskid <m255844@usna.edu>
2023-03-25 23:07:24 -0400

    msg

--- project2/ShiftCaesar.java
+++ project2/ShiftCaesar.java
@@ -4,11 +4,11 @@ public class ShiftCaesar implements Hash {
   public String hash(char[] pswd){
     Encryptor E = new Caesar();
     System.out.println(xxtend(pswd));
-    E.init(xxtend(pswd));
+    E.init((pswd));
     char[] x = initVec.toCharArray();
     System.out.println("base: " + new String(x));
     for(int i = 0; i < 16; i++){
-      int k = ((int)x[i] -42) %16;
+      int k = ((int)x[i]) %16;
       System.out.println("k: " + k + " i : " + i + " " + new String(x));
       x = shift(x, k);
       x = (E.encrypt(new String(x))).toCharArray();
