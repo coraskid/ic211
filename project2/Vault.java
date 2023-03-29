546d2214de0b9f0cc39e913aba0bc995ed2d142d
coraskid <m255844@usna.edu>
2023-03-29 14:59:11 -0400

    msg

---
+++ project2/Vault.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Vault {
+  
+  private class userHash {
+    private String user;
+    private String hash;
+    public userHash(String u, String h){
+      user = u;
+      hash = h;
+    }
+  }
+  
+  private ArrayList<userHash> data;
+
+  private readData(String 
+
+
+
+
+
+
+  public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+  }
+}
