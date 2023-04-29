4b179964dadfcb92ed9fce1cf2be269c69ac81e9
coraskid <m255844@usna.edu>
2023-04-29 10:44:15 -0400

    msg

--- project3/SFish.java
+++ project3/SFish.java
@@ -9,8 +9,9 @@ import javax.swing.*;
 public class SFish extends Animal {
   //MOVES UP DOWN LEFT RIGHT
   private int stepCount = 0;
-  private int direction;
+  private int dir;
   private BufferedImage img;
+  private final double MVE = .0001;
   public SFish(TankSize ts){
     super(ts);
     //depthGoal = rand.nextInt(820);
@@ -18,7 +19,7 @@ public class SFish extends Animal {
     this.x = 0;
     speed = 2;
     type = 2;
-    direction = 1;
+    dir = 1;
 
     BufferedImage image = null;
     try {
@@ -35,23 +36,61 @@ public class SFish extends Animal {
   }
 
   public void step(){
-    if ( x < ts.getHorz())
-      x += (speed * .0001);
+    if (dir == 1){
+      this.stepLeft();
+    } else if (dir == 3){
+      this.stepRight();
+    }
+    //FIX
+    //int vertMove = rand.next(25);
+    if(vertMove == 2){
+      this.stepUp();
+    }
+    if(vertMove == 3){
+      this.stepDown();
+    }
   }
+  private void stepLeft(){
+    x += (speed * MVE);
+    if( x >= 1) {
+      dir = 3;
+      x -= 2*(speed * MVE);
+      for(int i = 0; i < 10; i++)
+        this.stepDown();
+    }
+  }
+  private void stepRight(){
+    x -= (speed * MVE);
+    if(x <= 0) {
+      dir = 1;
+      x += 2*(speed * MVE);
+      for(int i = 0; i < 10; i++)
+        this.stepUp();
+    }
+  }
+  private void stepUp(){
+    if((y + (500 * MVE)) > 1)
+      y += (500 * MVE);
+  }
+  private void stepDown(){
+    if((y - (500 * MVE)) < 0)
+      y -= (500 *MVE);
+  }
+
 
   //private void stepleft(
-  
+
   public Shape getShape(){
     return new Rectangle2D.Double((int)(x*ts.getHorz()), 
-                                  (int)(y*ts.getVert()),
-                                  this.img.getWidth(),
-                                  this.img.getHeight());
+        (int)(y*ts.getVert()),
+        this.img.getWidth(),
+        this.img.getHeight());
   }
 
 
 
 
-  
+
 
 
 
