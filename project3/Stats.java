0bc266ca5be5fa9c1e4aafaf8217fce2f8217887
coraskid <m255844@usna.edu>
2023-04-30 16:56:04 -0400

    msg

---
+++ project3/Stats.java
@@ -0,0 +1,41 @@
+import java.awt.geom.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Stats implements JPanel, MouseListener {
+  private double clickX, clickY;
+  private Zoo zoo;
+
+
+  public Stats(Zoo zoo) {
+    this.zoo = zoo;
+    title = new JLabel("Fish Stats");
+    clear = new JButton("Clear Stats");
+    
+    
+
+    JPanel m = new JPanel();
+    m.setLayout(new BoxLayout(m, BoxLayout.PAGE_AXIS));
+    
+
+
+  }
+
+  public void setGoal(double gx, double gy) {
+    goalx = gx;
+    goaly = gy;
+  }
+
+  public void mouseClicked(MouseEvent e) {
+    setGoal(e.getX(), e.getY());
+  }
+
+  public void mouseEntered(MouseEvent e)  {}
+
+  public void mouseExited(MouseEvent e)   {}
+
+  public void mousePressed(MouseEvent e)  {}
+
+  public void mouseReleased(MouseEvent e) {}
+}
